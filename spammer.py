6ea3e53745c2135dca00eccf66327be49eaba076
HeroBaskan <98348835+HeroBaskan@users.noreply.github.com>
2022-03-27 18:29:32 +0300

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+import pyperclip
+
+time.sleep(5) #waiting 5 seconds to start the spam.
+
+pyperclip.copy('@')
+
+
+for i in range(9999): # you can change the number "9999" to any number. It indicates how much it will spam.
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.typewrite("everyone")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+    time.sleep(1) #spamming every 1 second. you can change this.
+
+
+
+
+
+
+
+
