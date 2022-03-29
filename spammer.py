2fb1385ab7ea26470016cbb316ccd2d0ef707089
HeroBaskan <98348835+HeroBaskan@users.noreply.github.com>
2022-03-29 20:27:49 +0300

    Update spammer.py
    fixed grammer issue :p

--- spammer.py
+++ spammer.py
@@ -7,7 +7,7 @@ time.sleep(5) #waiting 5 seconds to start the spam.
 pyperclip.copy('@')
 
 
-for i in range(9999): # you can change the number "9999" to any number. It indicates how much it will spam.
+for i in range(9999): # you can change the number "9999" to any number. It indicates how many times it will spam.
     pyautogui.hotkey('ctrl', 'v')
     pyautogui.typewrite("everyone")
     pyautogui.press("enter")
